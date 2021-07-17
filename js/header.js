c66ac475261f4a2b35511d25231d364737ee5c46
OksanaZhuravel <opuseva@mail.ru>
2021-07-17 19:34:26 +0300

    added new  blocks css

---
+++ js/header.js
@@ -0,0 +1,16 @@
+// // let head = document.querySelector(".header__account");
+// // let link = document.querySelectorAll(".header__items");
+
+// // // let item = head.querySelector("header__items");
+// // link.onclick("click", function (evt) {
+// //   evt.preventDefault();
+// //   console.log("click");
+// //   cl();
+// // });
+// function cl() {
+//   if (link.classList.contains("header__items--hover")) {
+//     link.classList.remove("header__items--hover");
+//   } else {
+//     link.classList.add("header__items--hover");
+//   }
+// }
