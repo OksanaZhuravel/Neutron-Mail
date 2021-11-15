efd4e5a2ebd223c0ebf2bd190d14d450b536d7be
OksanaZhuravel <opuseva@mail.ru>
2021-11-15 16:33:58 +0300

    js chanded

--- js/header.js
+++ js/header.js
@@ -1,19 +1,12 @@
-let account = document.querySelector('.header__account');
-let link = account.querySelectorAll('.header__items');
-// link.forEach(function (value) {
-//   console.log(value);
-// });
-let linkElem = null;
-let elem = function (evt) {
-  evt.preventDefault();
-  console.log('click');
-
-  if (linkElem) {
-    linkElem.classList.contains('header__items--hover');
-  }
-  linkElem = evt.currentTarget;
-  linkElem.classList.toggle('header__items--hover');
-};
-for (i = 0; i < link.length; i++) {
-  link[i].addEventListener('click', elem);
-}
+let headerClick = document
+  .querySelectorAll('.header__items')
+  .forEach(function (headerClick) {
+    console.log(headerClick);
+    headerClick.addEventListener('click', function () {
+      if (headerClick.classList.contains('header__items--hover')) {
+        headerClick.classList.toggle('header__items--hover');
+      } else {
+        headerClick.classList.add('header__items--hover');
+      }
+    });
+  });
